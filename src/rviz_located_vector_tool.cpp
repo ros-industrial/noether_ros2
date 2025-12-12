80b518766a02675ebc960b800cc83fe71d76ff77
Michael Ripperger <michael.ripperger@swri.org>
2025-12-11 22:25:14 -0600

    Copied Rviz2 measure tool; renamed to located vector tool

---
+++ src/rviz_located_vector_tool.cpp
@@ -0,0 +1,97 @@
+// Copyright (c) 2008, Willow Garage, Inc.
+// Copyright (c) 2018, Bosch Software Innovations GmbH.
+// All rights reserved.
+
+#include "rviz_located_vector_tool.h"
+
+#include <QString>  // NOLINT: cpplint is unable to handle the include order here
+#include <OgreSceneNode.h>
+#include <rviz_common/display_context.hpp>
+#include <rviz_common/load_resource.hpp>
+#include <rviz_common/viewport_mouse_event.hpp>
+#include <rviz_common/interaction/view_picker_iface.hpp>
+#include <rviz_common/properties/color_property.hpp>
+#include <rviz_common/properties/parse_color.hpp>
+#include <rviz_rendering/objects/line.hpp>
+
+namespace noether_ros
+{
+LocatedVectorTool::LocatedVectorTool()
+    : is_line_started_(false), length_(-1)
+{
+  shortcut_key_ = 'n';
+
+  color_property_ = new rviz_common::properties::ColorProperty(
+      "Line color", Qt::darkYellow,
+      "The topic on which to publish points.",
+      getPropertyContainer(), SLOT(updateLineColor()), this);
+}
+
+void LocatedVectorTool::onInitialize()
+{
+  line_ = std::make_shared<rviz_rendering::Line>(context_->getSceneManager());
+  updateLineColor();
+
+  std_cursor_ = rviz_common::getDefaultCursor();
+  hit_cursor_ = rviz_common::makeIconCursor("package://rviz_common/icons/crosshair.svg");
+}
+
+void LocatedVectorTool::activate()
+{
+  is_line_started_ = false;
+}
+
+void LocatedVectorTool::deactivate()
+{
+}
+
+int LocatedVectorTool::processMouseEvent(rviz_common::ViewportMouseEvent & event)
+{
+  Ogre::Vector3 pos;
+  bool success = context_->getViewPicker()->get3DPoint(event.panel, event.x, event.y, pos);
+  setCursor(success ? hit_cursor_ : std_cursor_);
+
+  if (is_line_started_ && success) {
+    line_->setPoints(start_, pos);
+    length_ = (start_ - pos).length();
+  }
+
+  if (event.leftUp() && success) {
+    processLeftButton(pos);
+    return Render;
+  }
+  if (event.rightUp()) {
+    processRightButton();
+  }
+
+  return 0;
+}
+
+void LocatedVectorTool::updateLineColor()
+{
+  Ogre::ColourValue color = rviz_common::properties::qtToOgre(color_property_->getColor());
+  line_->setColor(color);
+}
+
+void LocatedVectorTool::processLeftButton(const Ogre::Vector3 & pos)
+{
+  if (is_line_started_) {
+    end_ = pos;
+    line_->setPoints(start_, end_);
+    is_line_started_ = false;
+  } else {
+    start_ = pos;
+    is_line_started_ = true;
+  }
+}
+
+void LocatedVectorTool::processRightButton()
+{
+  is_line_started_ = false;
+  line_->setVisible(false);
+}
+
+}  // namespace noether_ros
+
+#include <pluginlib/class_list_macros.hpp>  // NOLINT
+PLUGINLIB_EXPORT_CLASS(noether_ros::LocatedVectorTool, rviz_common::Tool)
---
+++ src/rviz_located_vector_tool.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2008, Willow Garage, Inc.
+// Copyright (c) 2018, Bosch Software Innovations GmbH.
+// All rights reserved.
+
+# pragma once
+
+#include <memory>
+#include <QCursor>  // NOLINT cpplint cannot handle include order
+#include <OgreVector.h>
+#include <rviz_common/tool.hpp>
+
+namespace rviz_rendering
+{
+class Line;
+}
+
+namespace rviz_common
+{
+namespace properties
+{
+class ColorProperty;
+}
+}
+
+namespace noether_ros
+{
+class LocatedVectorTool : public rviz_common::Tool
+{
+  Q_OBJECT
+
+public:
+  LocatedVectorTool();
+
+  void onInitialize() override;
+
+  void activate() override;
+
+  void deactivate() override;
+
+  int processMouseEvent(rviz_common::ViewportMouseEvent & event) override;
+
+public Q_SLOTS:
+  void updateLineColor();
+
+private:
+  // void setStatusMessage();
+  void processLeftButton(const Ogre::Vector3 & pos);
+  void processRightButton();
+
+  rviz_common::properties::ColorProperty * color_property_;
+
+  std::shared_ptr<rviz_rendering::Line> line_;
+  Ogre::Vector3 start_;
+  Ogre::Vector3 end_;
+  bool is_line_started_;
+  float length_;
+
+  QCursor std_cursor_;
+  QCursor hit_cursor_;
+};
+
+}  // namespace noether_ros
