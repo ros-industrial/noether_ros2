19327e033143b9f794cb36aa72352af23c6566f4
Michael Ripperger <michael.ripperger@swri.org>
2025-12-29 14:23:45 -0600

    Added documentation to the located vector direction and origin generator classes

--- include/noether_ros/located_vector_generators.h
+++ include/noether_ros/located_vector_generators.h
@@ -7,9 +7,16 @@ FWD_DECLARE_YAML_STRUCTS()
 
 namespace noether_ros
 {
+/**
+ * @brief Calls a `noether_ros/srv/GetLocatedVector` service to generate the direction vector
+ */
 class LocatedVectorDirectionGenerator : public noether::DirectionGenerator
 {
 public:
+  /**
+   * @brief Constructor
+   * @param service_name Name of the service providing `noether_ros/srv/GetLocatedVector`
+   */
   LocatedVectorDirectionGenerator(const std::string& service_name);
 
   Eigen::Vector3d generate(const pcl::PolygonMesh& mesh) const override;
@@ -21,9 +28,16 @@ protected:
   std::string service_name_;
 };
 
+/**
+ * @brief Calls a `noether_ros/srv/GetLocatedVector` service to generate the origin location
+ */
 class LocatedVectorOriginGenerator : public noether::OriginGenerator
 {
 public:
+  /**
+   * @brief Constructor
+   * @param service_name Name of the service providing `noether_ros/srv/GetLocatedVector`
+   */
   LocatedVectorOriginGenerator(const std::string& service_name);
 
   Eigen::Vector3d generate(const pcl::PolygonMesh& mesh) const override;
